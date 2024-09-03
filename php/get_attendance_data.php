ede7e4d0f827f8108e81f148ec3dbdca28b9c779
Rylle Jalipa <rylle1588@gmail.com>
2024-09-03 13:20:02 +0800

    Fix Professor Dashboard Attendance Display

--- php/get_attendance_data.php
+++ php/get_attendance_data.php
@@ -10,11 +10,15 @@ if (!isset($_SESSION['prof_id'])) {
 $class_no = $_GET['class_no'];
 $date = $_GET['date'];
 
-$stmt = $conn->prepare("SELECT a.attendance_id, a.date, a.status, a.class_no, a.student_no, s.firstname AS first_name, s.lastname AS last_name
-                        FROM attendance a
-                        INNER JOIN student s ON a.student_no = s.student_number
-                        WHERE a.class_no = ? AND a.date = ?");
-$stmt->bind_param("ss", $class_no, $date);
+$stmt = $conn->prepare("
+    SELECT s.student_number, s.firstname AS first_name, s.lastname AS last_name, a.status
+    FROM class c
+    INNER JOIN student s ON c.student_no = s.student_number
+    LEFT JOIN attendance a ON a.student_no = c.student_no AND a.class_no = c.class_no AND a.date = ?
+    WHERE c.class_no = ?
+    GROUP BY s.student_number
+");
+$stmt->bind_param("ss", $date, $class_no);
 $stmt->execute();
 $result = $stmt->get_result();
 
@@ -22,13 +26,10 @@ $attendance_data = [];
 
 while ($row = $result->fetch_assoc()) {
     $attendance_data[] = array(
-        'attendance_id' => $row['attendance_id'],
-        'date' => $row['date'],
-        'status' => $row['status'],
-        'class_no' => $row['class_no'],
-        'student_no' => $row['student_no'],
+        'student_no' => $row['student_number'],
         'first_name' => $row['first_name'],
-        'last_name' => $row['last_name']
+        'last_name' => $row['last_name'],
+        'status' => $row['status'] // Status can be null if there's no attendance record yet
     );
 }
 
